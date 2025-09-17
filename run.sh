d61d82ab098ba2ce30eb91f3df96d351cddf3b3c
tech-pius <piusdenistech@gmail.com>
2025-09-17 10:31:49 +0300

    fix: update run.sh to default APP_HOST to 0.0.0.0 for external server access

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 export $(grep -v '^#' .env | xargs -d '\n' -r)
-export APP_HOST=${APP_HOST:-127.0.0.1}
+export APP_HOST=${APP_HOST:-0.0.0.0}
 uvicorn main:app --host $APP_HOST --port ${APP_PORT:-8000} --reload
